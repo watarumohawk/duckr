691fd939b5fb09708b0ab83feb35b442fcbe3560
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2017-07-25 23:49:28 +0900

    authenticate mock

--- app/components/index.js
+++ app/components/index.js
@@ -1,2 +1,4 @@
 export Home from './Home/Home'
 export Navigation from './Navigation/Navigation'
+export AuthenticateContainer from './Authenticate/Authenticate'
+export FacebookAuthButton from './FacebookAuthButton/FacebookAuthButton'
