cb04c6591ca13842a7e6fd0734a28943937492a0
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2017-08-06 14:58:35 +0900

    fixed a typo

--- app/components/index.js
+++ app/components/index.js
@@ -1,4 +1,4 @@
 export Home from './Home/Home'
 export Navigation from './Navigation/Navigation'
-export AuthenticateContainer from './Authenticate/Authenticate'
+export Authenticate from './Authenticate/Authenticate'
 export FacebookAuthButton from './FacebookAuthButton/FacebookAuthButton'
